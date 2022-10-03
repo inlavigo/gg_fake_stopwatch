f2b2fd8df7376c12a6694d53c8082a9b5de32ab1
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2022-10-03 12:07:31 +0200

    Clean dart package

---
+++ example/gg_fake_stopwatch_example.dart
@@ -0,0 +1,6 @@
+import 'package:gg_fake_stopwatch/gg_fake_stopwatch.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/gg_fake_stopwatch.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library gg_fake_stopwatch;
+
+export 'src/gg_fake_stopwatch_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
